48d4aaf47e093933b28432bc8315d657b305fb6f
Julio Xavier <julioxavierr@live.com>
2017-06-14 01:04:24 -0300

    Increasing lenght dictionary
    Signed-off-by: Lucas Andrade <lucasandradeunb@gmail.com>

--- lib/inox_converter/length.rb
+++ lib/inox_converter/length.rb
@@ -4,9 +4,17 @@ module InoxConverter
 		def initDictionary
       @dictionary = Hash.new
       @dictionary = {
-        "mm" => 0.001,
+        "km" => 1000,
+        "hm" => 100,
+        "dam" => 10,
         "m" => 1,
-        "km" => 1000
+        "dm" => 0.1,
+        "cm" => 0.01,
+        "mm" => 0.001,
+        "mile" => 1609.34,
+        "yard" => 0.9144,
+        "feet" => 0.3048,
+        "count" => 0.0254
       }
     end
 		
