80b1d4ac912b394292dae71ad43a31ffb0dccd56
Jhonatan Alves <jhon_fla@hotmail.com>
2017-06-17 18:53:10 -0300

    Adding mass units abbraviation and corrections
    Signed-off-by: nicacioneto <nicacionetobsb@hotmail.com>

--- lib/inox_converter/mass.rb
+++ lib/inox_converter/mass.rb
@@ -6,14 +6,23 @@ module InoxConverter
       @dictionary = Hash.new
       @dictionary = {
         "tonne" => 10**6,
+        "metric ton" => 10**6,
+        "t" => 10**6,
         "kilogram" => 1000,
+        "kg" => 1000,
         "gram" => 1,
+        "g" => 1,
         "miligram" => 0.001,
+        "mg" => 0.001,
         "microgram" => 10**-6,
         "imperial ton" => 1016000,
         "us ton" => 907185,
         "stone" => 6350.29,
-        "pound" => 453.592
+        "st" => 6350.29,
+        "pound" => 453.592,
+        "lb" => 453.592,
+        "ounce" => 28.349,
+        "oz" => 28.349
       }
     end
 
