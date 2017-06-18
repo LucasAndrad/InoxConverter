e2341365b7c92c10b23ab8cabd283f7020b065b8
nicacioneto <nicacionetobsb@hotmail.com>
2017-06-18 10:19:38 -0300

    Adding first test of InoxConverter Module
    Signed-off-by: vinisilvacar <vinisilvacar@gmail.com>

--- spec/inox_converter_spec.rb
+++ spec/inox_converter_spec.rb
@@ -5,3 +5,12 @@ RSpec.describe InoxConverter do
     expect(described_class).to equal(InoxConverter)
   end
 end
+
+RSpec.describe InoxConverter do
+  describe "instantiation" do
+    it "should run convertCurrency" do
+      @inox_converter = InoxConverter.convertCurrency(10,'firstUnit', 'secondUnit')
+      expect('Currency')
+    end
+  end
+end
