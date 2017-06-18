0c6dcfd1b770b9e588b18e804a8377ab0518e699
GesielFreitas <gesiel.was.f@gmail.com>
2017-06-18 15:38:02 -0300

    fixed erros for not return api
    Signed-off-by: GesielFreitas <gesiel.was.f@gmail.com>
    Signed-off-by: vinisilvacar <vinisilvacar@gmail.com>

--- lib/inox_converter/api/api.rb
+++ lib/inox_converter/api/api.rb
@@ -3,39 +3,75 @@ require "active_support/core_ext/hash"
 module Api
 	class Api
 
-		dados = ''
+		@dados = ''
+		@data = true
 
 		#Consuming yahoo finances api and transform in hash for ruby
-		def consumirApi
+		def consume_api
 			@dados = RestClient::Request.execute(method: :get, url: 'https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote')
 			hash_local = Hash.new
 			@hash_local = Hash.from_xml(@dados)
 		end
 
 		#Treating data in hash
-		def tratarDados()
+		def treat_data
 			@hash_inter = Hash.new
 			@hash = Hash.new
-			@hash_inter = @hash_local['list']['resources']['resource']
-			@hash_inter.each do |cout|
-				simbol_string = cout['field'][0].to_s
-				simbol = simbol_string.split("/")
-			 	@hash[simbol[1]] = cout['field'][1].to_f
-	 		end	
+			if validate_api_return
+				@hash_inter = @hash_local['list']['resources']['resource']
+				@hash_inter.each do |cout|
+					simbol_string = cout['field'][0].to_s
+					simbol = simbol_string.split("/")
+				 	@hash[simbol[1]] = cout['field'][1].to_f
+		 		end
+		 	else
+ 				@data = false
+ 			end	
+		end
+
+		#verifield return of consumir api
+		def validate_api_return
+			if @dados.nil?
+				return false
+			else
+				return true
+			end
+		end
+
+		#verifield value in @hash for calcule currency convert
+		def return_hash_currency(valor)
+			if @hash[valor].nil?
+				return false
+			else
+				return true
+			end		
+		end
+
+		def data_validate_api(firstUnit, secondUnit)
+			if @data == false || return_hash_currency(firstUnit) == false || return_hash_currency(secondUnit) == false
+			return false
+			else
+				return true
+			end 
 		end
 
 		#Template of execution sequence of the methods and return @hash
-		def dictionary_api
-			consumirApi
-			tratarDados
-			return @hash
+		def dictionary_api(firstUnit, secondUnit)
+			consume_api
+			treat_data
+			data_validate_api(firstUnit, secondUnit)
+			
 		end
 
 		#new metodo for convert currency
 		def convert_currency(valueToConvert, firstUnit, secondUnit)
-			dictionary_api
-			finalValue = (valueToConvert / @hash[firstUnit]) * @hash[secondUnit]
-			return finalValue
+			
+			if dictionary_api(firstUnit, secondUnit) == false
+				return 0
+			else	 
+				finalValue = (valueToConvert / @hash[firstUnit]) * @hash[secondUnit]
+				return finalValue
+			end
 		end 
 	end
 end
