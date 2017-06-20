2fa0213f5376d9ea03444cdc4217b4c5b0088102
GesielFreitas <gesiel.was.f@gmail.com>
2017-06-19 22:38:03 -0300

    unit tests for return_has_currency in hash api and treat data

--- spec/api/api_spec.rb
+++ spec/api/api_spec.rb
@@ -1,10 +1,14 @@
+require 'spec_helper'
 require 'inox_converter/api/api'
+
 
 RSpec.describe Api::Api, type: :class do
 
 	describe "Testing consuming api" do 
 		before do
 			@dados = RestClient::Request.execute(method: :get, url: 'https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote')
+			hash_local = Hash.new
+			@hash_local = Hash.from_xml(@dados)
 		end
 
 		it 	"testing method consume api" do
@@ -12,16 +16,46 @@ RSpec.describe Api::Api, type: :class do
 			@hash = api.consume_api
 			expect(Hash.new)
 		end
+
+		it "method tread data in api for created dicionary data fo currency dados" do 
+			api = Api::Api.new
+			@hash = api.treat_data
+			expect(@hash_local)
+		end
 	end
 
 	describe "testing validates in api" do
 		before do
 			@dados = RestClient::Request.execute(method: :get, url: 'https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote')
-		end    
+			hash_local = Hash.new
+			@hash_local = Hash.from_xml(@dados)
+
+		end
+		  
 		it "should run validate metod api return" do
 			api = Api::Api.new
 			validate =  api.validate_api_return
 			expect(true)   
 		end
+
+		it "should run validate return_hash_currency" do
+			valor_2 = "BRL"
+			api = Api::Api.new
+			@hash_local = api.consume_api
+			@hash = api.treat_data
+			validate =  api.return_hash_currency(valor_2)
+			expect(true)   
+		end
+
+		it "should run validate return_hash_currency in false return" do
+			valor_1 = "GSFD"
+			api = Api::Api.new
+			@hash_local = api.consume_api
+			@hash = api.treat_data
+			validate =  api.return_hash_currency(valor_1)
+			expect(false)   
+		end
+
+		
 	end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'simplecov'
 SimpleCov.start
 require 'inox_converter'
+require 'inox_converter/api/api'
 RSpec.configure do |config|
   # some (optional) config here
 end
