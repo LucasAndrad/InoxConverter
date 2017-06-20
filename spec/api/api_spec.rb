d3d9aa059f8cf01076c188b1deec0a0987dd3b15
GesielFreitas <gesiel.was.f@gmail.com>
2017-06-20 00:14:13 -0300

    Fixed unit tests for convert currency in api
    Signed-off-by: GesielFreitas <gesiel.was.f@gmail.com>
    Signed-off-by: vinisilvacar <vinisilvacar@gmail.com>

--- spec/api/api_spec.rb
+++ spec/api/api_spec.rb
@@ -26,67 +26,84 @@ RSpec.describe Api::Api, type: :class do
 
 	describe "testing validates in api" do
 		before do
-			@dados = RestClient::Request.execute(method: :get, url: 'https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote')
-			hash_local = Hash.new
-			@hash_local = Hash.from_xml(@dados)
+			@api = Api::Api.new
+			@hash_local = @api.consume_api
+			@hash = @api.treat_data
 
 		end
 		  
 		it "should run validate metod api return" do
-			api = Api::Api.new
-			validate =  api.validate_api_return
+			validate =  @api.validate_api_return
 			expect(true)   
 		end
 
 		it "should run validate return_hash_currency" do
 			valor_2 = "BRL"
-			api = Api::Api.new
-			@hash_local = api.consume_api
-			@hash = api.treat_data
-			validate =  api.return_hash_currency(valor_2)
+			validate =  @api.return_hash_currency(valor_2)
 			expect(true)   
 		end
 
 		it "should run validate return_hash_currency in false return" do
 			valor_1 = "GSFD"
-			api = Api::Api.new
-			@hash_local = api.consume_api
-			@hash = api.treat_data
-			validate =  api.return_hash_currency(valor_1)
+			validate =  @api.return_hash_currency(valor_1)
 			expect(false)   
 		end
 
 		it "should run validate in data_validate_api for true" do
-			api = Api::Api.new
-			@hash_local = api.consume_api
-			@hash = api.treat_data
-			validate = api.data_validate_api("BRL", "EUR")
+			validate = @api.data_validate_api("BRL", "EUR")
 			expect(true)
 		end
 
 		it "should run validate in data_validate_api for false" do
-			api = Api::Api.new
-			@hash_local = api.consume_api
-			@hash = api.treat_data
-			validate = api.data_validate_api("BRL", "GDSF")
+			validate = @api.data_validate_api("BRL", "GDSF")
 			expect(false)
 		end
 
-		it "should run validate in validate_first_unit for true" do
-			api = Api::Api.new
-			@hash_local = api.consume_api
-			@hash = api.treat_data
-			validate = api.validate_first_unit("BRL")
+		 it "should run validate in validate_first_unit for true" do
+			validate = @api.validate_first_unit("BRL")
 			expect(false)
 		end
 
 		it "should run validate in validate_first_unit for false" do
-			api = Api::Api.new
-			@hash_local = api.consume_api
-			@hash = api.treat_data
-			validate = api.validate_first_unit("GDSF")
+			validate = @api.validate_first_unit("GDSF")
+			expect(false)
+		end
+
+		it "should run validate in validate_second_unit for USD true" do
+			validate = @api.validate_second_unit("USD")
+			expect(false)
+		end
+
+		it "should run validate in validate_second_unit for USD false" do
+			validate = @api.validate_second_unit("BRL")
 			expect(false)
 		end
 		
+	end
+	describe "Testing convert_currency in api" do 
+		before do
+			@api = Api::Api.new
+		end
+		
+		it "should run convert_currency for second unit is USD and firs unit that in hash" do
+			valor = @api.convert_currency(20, "EUR", "USD")
+			expect(valor).to be_instance_of(Float)
+			expect(valor).to be >  20
+		end
+
+		it "should run convert_currency for second unit is USD and firs unit It is not on hash" do
+			valor = @api.convert_currency(20, "GDSF", "USD")
+			expect(valor).to eq(0)
+		end
+
+		it "should run convert_currency for second unit is not USD and two unit that in hash" do
+			valor = @api.convert_currency(20, "EUR", "BRL")
+			expect(valor).to be_instance_of(Float)
+			expect(valor).to be >  20
+		end
+		# it "should run validate in validate_second_unit for USD false" do
+		# end
+		# it "should run validate in validate_second_unit for USD false" do
+		# end
 	end
 end
